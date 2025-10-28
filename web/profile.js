0d1c89483b1bb26da66876f3ce1c7e9b8f284665
dhanush-business <dhanush.business01@gmail.com>
2025-10-28 17:08:56 +0530

    Update profile.js

--- web/profile.js
+++ web/profile.js
@@ -1,5 +1,5 @@
 // --- Backend URL ---
-const BACKEND_URL = ''; // Relative path
+const BACKEND_URL = 'https://luvisa-render.onrender.com'; // Relative path
 
 // --- Default Avatar Path (Served by Frontend/Vercel) ---
 const DEFAULT_AVATAR_STATIC_PATH = "/avatars/default_avatar.png";
@@ -164,5 +164,6 @@ async function saveProfileChanges() {
     }
 
 }
+
 
 
